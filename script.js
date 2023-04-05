233d6cb8039cd03274805f3392afbd1d78e41e7b
Bernardo Cavalcanti <bernardogamacavalcanti@gmail.com>
2023-04-05 16:20:10 -0300

    logica completa, falta estilizar

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const password = document.querySelector('#field')
+const button = document.querySelector('button')
 
+let passwordLenght = 8
+let characters = 'abcdefghijklmnopqrstuvwxysABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%¨&*()-_=/*-+.?|,<>;:/[]{}~^'
+let passwordValue = ''
+
+
+const createPassword = () => {
+
+    passwordValue = ''
+
+    for(let i = 0; i < passwordLenght; i++) {
+    let number = Math.floor(Math.random() * characters.length)
+    passwordValue += characters.substring(number, number + 1)
+    }
+
+    password.value = passwordValue
+}
+
+button.addEventListener('click', createPassword)
